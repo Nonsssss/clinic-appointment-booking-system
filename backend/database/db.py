d275cfeef3c8e47087314dd642bf75245f514d29
John <johnbenedictvianneysolanoanire@gmail.com>
2026-05-17 14:19:11 +0800

    Local db

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,18 +1,8 @@
-import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv 
 
-
-load_dotenv()
-
-DATABASE_URL = (
-    
-    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:"
-    f"{os.getenv('MYSQLPASSWORD')}@"
-    f"{os.getenv('MYSQLHOST')}/"
-    f"{os.getenv('MYSQLDATABASE')}"
-)
+DATABASE_URL = "mysql+pymysql://root:admin123!@localhost:3306/clinic_booking_system"
   
 
 
